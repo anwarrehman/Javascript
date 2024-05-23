4106823cabeff5ed23e6e64e1bb2f2bc0ec87537
anwarrehman <ar1498137@gmail.com>
2024-05-23 04:26:36 -0700

    variables for numbers

---
+++ chapter-03/app.js
@@ -0,0 +1,25 @@
+// let num = 27;
+// alert(num);
+
+
+
+
+
+
+
+
+// let num1 = 10;
+// let num2 = 10;
+// let add = num1 + num2;
+// alert(add);
+
+
+
+
+
+
+
+
+// let num = 5;
+// let addNum = num + 10;
+// alert(addNum);
