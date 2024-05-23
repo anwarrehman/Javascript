37ebe8f709300f7c6ec3dad828b6b1fbf5800efc
anwarrehman <ar1498137@gmail.com>
2024-05-23 04:17:00 -0700

    variables for strings

---
+++ chapter-02/app.js
@@ -0,0 +1,32 @@
+// let userName = "Muhammad Anwar";
+// alert(userName);
+
+
+
+
+
+
+
+
+// let userName = "Anwar";
+// alert(userName);
+
+
+
+
+
+
+
+// let userName = "Muhammad";
+// userName = "Anwar";
+// alert(userName);
+
+
+
+
+
+
+
+
+// let thanx = "Thanks for your input!";
+// alert(thanx);
