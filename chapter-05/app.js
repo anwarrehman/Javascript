7b81792f263449ed2745f71b9abac3b1f7ea1b9a
anwarrehman <ar1498137@gmail.com>
2024-05-23 04:47:11 -0700

    math operators

---
+++ chapter-05/app.js
@@ -0,0 +1,90 @@
+// let add = 2 + 2;
+// alert(add);
+
+
+
+
+
+
+
+
+// let sub = 3 - 3;
+// alert(sub);
+
+
+
+
+
+
+
+
+
+// let mul = 4 * 4;
+// alert(mul);
+
+
+
+
+
+
+
+
+
+
+
+// let devide = 5 / 5;
+// alert(devide);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let modul = 6 % 6;
+// alert(modul);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let all = 2 + 2 - 3 * 4 / 5 % 6;
+// alert(all);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let sub = 2 - 20;
+// alert(sub);
