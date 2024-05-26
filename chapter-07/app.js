826d15d90fa4bae1da099134605d1d138cf0b15f
anwarrehman <ar1498137@gmail.com>
2024-05-26 01:50:25 -0700

    math eliminating ambiguity

---
+++ chapter-07/app.js
@@ -0,0 +1,40 @@
+// let totalNum = 1 + 3 * 4;
+// alert(totalNum);
+
+
+
+
+
+
+
+
+// let totalNum = 1 + (3 * 4);
+// alert(totalNum);
+
+
+
+
+
+
+
+
+
+// let result = (2 * 4) * 4 + 2;
+// alert(result);
+
+
+
+
+
+
+
+
+
+
+
+
+
+let result = ((2 * 4) * 4) + 2;
+alert(result);
+
+
