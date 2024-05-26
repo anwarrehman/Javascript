42bff81d5a556a04e8e85d59c02d0860a630aa29
anwarrehman <ar1498137@gmail.com>
2024-05-26 02:23:13 -0700

    if statement

---
+++ chapter-10/app.js
@@ -0,0 +1,20 @@
+// let ques = prompt("Where do you live?");
+// if (ques === "karachi"){
+//     alert("Ok");
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+// let cityName = prompt("What is your city name?");
+// if (cityName === "karachi"){
+//     alert("Welcome to city of lights");
+// }
