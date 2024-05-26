d3f8f7e31dd6ec9bc5d73421779d05b67672a585
anwarrehman <ar1498137@gmail.com>
2024-05-26 02:10:18 -0700

    prompts

---
+++ chapter-09/app.js
@@ -0,0 +1,32 @@
+// let userName = prompt("What is your name?");
+
+
+
+
+
+
+
+
+
+
+// let ques = "What is your name?";
+// let randomAns = "Anwar";
+// let spec = prompt(ques, randomAns);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let numOfCycles = prompt("How many cycles?");
+// let tooManyCycles = numOfCycles + 1;
