03d4af3611ccfdaa3ccc5d9eb86274b1941011b9
anwarrehman <ar1498137@gmail.com>
2024-05-26 02:01:08 -0700

    concatenating text strings

---
+++ chapter-08/app.js
@@ -0,0 +1,70 @@
+// let message = "Hello World";
+// alert(message);
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let userName = "Muhammad Anwar";
+// alert("Assalam o alaikum " + userName + " !");
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let message = "Hello";
+// let userName = "Anwar";
+// let banger = "!";
+// alert(message + userName + banger);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// alert("5" + "5");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// alert("2 plus 2 equals " + 2 + 2);
