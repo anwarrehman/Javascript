c7331e9ba0ccab9e979e8effd4c9c866ea24d69e
anwarrehman <ar1498137@gmail.com>
2024-05-27 11:10:51 -0700

    sets of condition

---
+++ chapter-13/app.js
@@ -0,0 +1,23 @@
+// let weight = prompt("Enter your weight.");
+
+// let age = prompt("Enter you age.");
+
+// let gender = prompt("Enter gender.");
+
+// let country = prompt("Enter your country name.");
+
+
+// if (weight < 90
+//     && 
+//     age < 50 
+//     &&
+//     age > 20 
+//     && 
+//     gender === "male" || gender === "female"
+//     &&
+//     country === "pakistan"){
+//     alert("Conrats! You are eligible for job.");
+// }
+// else{
+//     alert("Sorry! You are not eligilbe for job.");
+// }
