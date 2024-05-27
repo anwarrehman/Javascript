b69f300c6dde97600fd77cf785d73989cc3cc974
anwarrehman <ar1498137@gmail.com>
2024-05-27 10:38:50 -0700

    comparison operators

---
+++ chapter-11/app.js
@@ -0,0 +1,49 @@
+// alert("Muhammad" + " " + "Anwar");
+
+
+
+
+
+
+
+
+
+// if (1 > 0){
+//     alert("You'r write");
+// }
+
+
+
+
+
+
+
+// if (0 < 1){
+//     alert("You'r not write");
+// }
+
+
+
+
+
+
+
+
+// if (1 >= 0){
+//     alert("You'r not write");
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+if (0 >= 0){
+    alert("You'r not write");
+}
