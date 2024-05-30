020e33f17a32e55b408d83f0b71db4321badfd8e
anwarrehman <ar1498137@gmail.com>
2024-05-30 04:05:55 -0700

    basic array methods

---
+++ chapter-17/app.js
@@ -0,0 +1,66 @@
+// let arr = ["Muhammad","Anwar",24,true];
+
+// arr.unshift("Hafiz");
+
+// arr.shift();
+
+// alert(arr);
+
+
+
+
+// let array = [1,2,3,4,5];
+
+// array.push(6,7,8,9,10);
+
+// array.pop();
+
+// array.unshift(0);
+
+// array.shift();
+
+// console.log(array);
+
+
+
+
+
+
+
+
+// let array = ['Muhammad','Anwar',10,true];
+
+// array.splice(3,1,'abc');
+
+// console.log(array);
+
+
+
+
+
+
+
+
+
+// let arr = [1,2,3,4,5];
+
+// let arr2 = arr.slice(0,3);
+
+// console.log(arr2);
+
+
+
+
+
+
+
+
+
+
+
+// push = add last index;
+// pop = remove last index;
+// unshift = add start index;
+// shift = remove last index;
+// splice (0,0, 'abc'); = add & remove all indexes;
+// slice (0, n-1); = copy;
