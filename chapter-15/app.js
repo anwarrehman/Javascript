de6bb63b47a5aefd7d38b800126cbc33427a4f1d
anwarrehman <ar1498137@gmail.com>
2024-05-30 02:23:23 -0700

    arrays

---
+++ chapter-15/app.js
@@ -0,0 +1,26 @@
+// let daysName = ['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday','Sunday'];
+// alert(daysName[0]);
+// alert(daysName[1]);
+// alert(daysName[2]);
+// alert(daysName[3]);
+// alert(daysName[4]);
+// alert(daysName[5]);
+// alert(daysName[6]);
+
+
+
+
+
+
+
+
+
+
+
+
+// let countryName = ['Pakistan','Afghanistan','Australia','Germany','Qatar'];
+// alert("Welcome to " + countryName[0]);
+// alert("Welcome to " + countryName[1]);
+// alert("Welcome to " + countryName[2]);
+// alert("Welcome to " + countryName[3]);
+// alert("Welcome to " + countryName[4]);
