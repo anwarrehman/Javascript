ffeb34e6af0ac14ad30d87c7da5ac4772c666835
anwarrehman <ar1498137@gmail.com>
2024-05-30 03:39:42 -0700

    arrays remove & add elements

---
+++ chapter-16/app.js
@@ -0,0 +1,27 @@
+// let daysName = [];
+
+// daysName.push("Monday");
+// daysName.push("Tuesday");
+// daysName.push("Wednesday");
+// daysName.push("Thursday");
+// daysName.push("Friday");
+// daysName.push("Saturday");
+// daysName.push("Sunday");
+
+// alert(daysName[0]);
+// alert(daysName[1]);
+// alert(daysName[2]);
+// alert(daysName[3]);
+// alert(daysName[4]);
+// alert(daysName[5]);
+// alert(daysName[6]);
+
+
+// daysName.pop();
+// daysName.pop();
+// daysName.pop();
+// daysName.pop();
+// daysName.pop();
+// daysName.pop();
+
+// alert(daysName);
