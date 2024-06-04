387ddc8d22711a93eacd61bc7c0ba31aaff18595
anwarrehman <ar1498137@gmail.com>
2024-06-04 03:33:53 -0700

    for loop

---
+++ chapter-18/app.js
@@ -0,0 +1,50 @@
+// for () {
+    // loop
+// }
+
+
+
+
+
+
+
+// for (let i = 1; i <= 10; i++){
+//     console.log(i);
+// }
+
+
+
+
+
+
+
+// for (let i = 1; i <= 20; i++){
+//     console.log(i);
+// }
+
+
+
+
+
+
+
+// for (let i = 1; i <= 50; i++){
+//     console.log(i);
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// for (let i = 1; i <= 200; i++){
+//     console.log(i);
+// }
