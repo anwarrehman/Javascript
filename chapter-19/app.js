7cf70dd86c71cef468c01e40edf6b545ce2b281d
anwarrehman <ar1498137@gmail.com>
2024-06-04 03:59:32 -0700

    for loop: array length

---
+++ chapter-19/app.js
@@ -0,0 +1,18 @@
+// let cityNames = ['Karachi', 'Lahore', 'Rawalpindi', 'Multan', 'Faisalabad'];
+
+// for (let i = 0; i < cityNames.length; i++){
+//     console.log(cityNames[i]);
+// }
+
+
+
+
+
+let countryNames = ['Pakistan', 'Afghanistan', 'Australia', 'Germany', 'Hong Kong'];
+
+// console.log(countryNames);
+
+for (let i = 0; i <= 4; i++){
+    countryNames[i] += ' Country';
+    console.log(countryNames[i]);
+}
