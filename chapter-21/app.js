ef38c6a54020028472f2db9bc139df2c7d5972c4
anwarrehman <ar1498137@gmail.com>
2024-06-05 02:23:26 -0700

    change case

---
+++ chapter-21/app.js
@@ -0,0 +1,25 @@
+// let userName = "MuHaMmaD anWaR";
+
+// let chgCase = userName.toLowerCase();
+
+// console.log(chgCase);
+
+
+
+
+
+// let userName = "MuHaMmaD anWaR";
+
+// let chgCase = userName.toUpperCase();
+
+// console.log(chgCase);
+
+
+
+
+
+let userName = prompt("Enter your name.");
+
+if (userName === userName.toUpperCase()){
+    console.log("thanks")
+}
