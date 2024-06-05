0309c4ba7051ec524ab621c2f0df7cc9278a83ae
anwarrehman <ar1498137@gmail.com>
2024-06-05 02:14:05 -0700

    for loop nested

---
+++ chapter-20/app.js
@@ -0,0 +1,30 @@
+// let firstName = ['Blueray', 'Upchuck', 'Lojack', 'Gizmo', 'Do-rag'];
+// let lastName = ['Zzz', 'Burp', 'Dogbone', 'Droop'];
+// let fullName = [];
+
+// for (let i = 0; i < firstName.length; i++){
+//     for (let j = 0; j < lastName.length; j++){
+//         fullName.push(firstName[i] + lastName[j]);
+//         console.log(fullName);
+//     }
+// }
+
+
+
+
+
+
+
+
+
+
+// let prayersName = ['Fajar', 'Zuhar', 'Asar', 'Magrib', 'Isha'];
+// let addName = ['Namaz'];
+// let fullName = [];
+
+// for (let i = 0; i < prayersName.length; i++){
+//     for (let j = 0; j < addName.length; j++){
+//         fullName.push(prayersName[i] + ' ' + addName[j]);
+//         console.log(fullName);
+//     }
+// }
