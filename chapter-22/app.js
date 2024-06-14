8a23cf34ac44a15466241210a4b79efd1eb5f3e3
anwarrehman <ar1498137@gmail.com>
2024-06-14 14:17:02 -0700

    measuring & extracting parts

---
+++ chapter-22/app.js
@@ -0,0 +1,30 @@
+// let str = prompt('Enter some text.');
+
+// let numChars = str.length;
+
+// for (let i = 0; i < numChars; i++){
+//     if (str.slice(i, i + 2) === "  "){
+//         alert("No double spaces!");
+//         break;
+//     }
+// }
+
+
+
+
+
+
+
+
+
+
+// let num = prompt('Enter some num.');
+
+// let numChars = num.length;
+
+// for (let i = 0; i < numChars; i++){
+//     if (num.slice(i, i + 1) === " "){
+//         alert("No space!");
+//         break;
+//     }
+// }
