dc599310511e9d0fed26ef1654a4e97001d2c5a6
anwarrehman <ar1498137@gmail.com>
2024-06-21 10:59:28 -0700

    find chr at location

---
+++ chapter-24/app.js
@@ -0,0 +1,28 @@
+// let firstName = 'Muhammad';
+
+// let lastName = 'Anwar';
+
+// let firstChr = firstName.charAt(0);
+
+// let lastChr = lastName.charAt(lastName.length - 1);
+
+// console.log(firstChr);
+// console.log(lastChr);
+
+
+
+
+
+
+
+
+
+
+// let text = "the quick world war II brown fox jumps world war II over the lazy world war II dog !";
+
+// for (let i = 0; i < text.length; i++) {
+//     if (text.charAt(i) === '!') {
+//         console.log('Exclamation point found!');
+//         break;
+//     }
+// }
