2a135ddc9d0b388988ae27374c1561dc7ebc9273
anwarrehman <ar1498137@gmail.com>
2024-06-21 11:09:00 -0700

    replace chrs

---
+++ chapter-25/app.js
@@ -0,0 +1,22 @@
+// let text = "the quick world war II brown fox jumps world war II over the lazy world war II dog !";
+
+// let newText = text.replace("world war II", "the second world war");
+
+// console.log(text);
+
+// console.log(newText);
+
+
+
+
+
+
+
+
+// let text = "the quick world war II brown fox jumps world war II over the lazy world war II dog !";
+
+// let newText = text.replace(/world war II/g, "the second world war");
+
+// console.log(text);
+
+// console.log(newText);
