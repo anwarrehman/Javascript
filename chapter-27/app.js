fd17dfcfee29bd8dbf9dfcc886905bf283da6d5c
anwarrehman <ar1498137@gmail.com>
2024-06-23 11:06:25 -0700

    generate rndm num

---
+++ chapter-27/app.js
@@ -0,0 +1,43 @@
+// let num = Math.random();
+
+// console.log(num); 
+// console.log(Math.random(num)); // same result
+
+
+
+
+
+
+
+
+
+
+
+// let num = Math.ceil(Math.random() * 6);
+
+// console.log(num);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let num = Math.floor(Math.random() * 6);
+
+// console.log(num);
