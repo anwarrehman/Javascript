0f19a95b066cd64a5a29edefc1860e178ae5340c
anwarrehman <ar1498137@gmail.com>
2024-06-23 11:41:03 -0700

    control the length of decimal

---
+++ chapter-30/app.js
@@ -0,0 +1,19 @@
+// let num = Math.random();
+
+// console.log(num.toFixed());
+
+
+
+
+
+
+
+
+
+
+
+
+// let num1 = 2.332;
+// let num2 = 3.239;
+
+// console.log(num1.toFixed(), num2.toFixed(), num1.toString(), num2.toString());
