cdcb9c77c2447f4862e4adc525cb88fc0d8e08bf
anwarrehman <ar1498137@gmail.com>
2024-06-23 11:29:08 -0700

    convert num to str & str to num

---
+++ chapter-29/app.js
@@ -0,0 +1,59 @@
+// let str = "18";
+
+// console.log(str);
+// console.log(Number(str));
+
+
+
+
+
+
+
+
+// let str1 = "9.382";
+// let str2 = "1.321";
+
+// console.log(Number(str1), Number(str2));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let num = 12345;
+
+// console.log(num)
+// console.log(num.toString());
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let num1 = 123;
+// let num2 = 456;
+
+// console.log(num1.toString(), num2.toString(), Number(num1), Number(num2));
