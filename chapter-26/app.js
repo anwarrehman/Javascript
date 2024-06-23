283583c37acb97c4cbdab56e7bf05f4f7a61253c
anwarrehman <ar1498137@gmail.com>
2024-06-23 10:59:49 -0700

    round numbers

---
+++ chapter-26/app.js
@@ -0,0 +1,68 @@
+// let num1 = 59.23;
+// let num2 = 59.53;
+
+// console.log(Math.round(num1));
+// console.log(Math.round(num2));
+
+
+
+
+
+
+
+
+
+
+// let num1 = 2.4;
+// let num2 = 2.5;
+// let num3 = 2.6;
+
+// console.log(Math.round(num1));
+// console.log(Math.round(num2));
+// console.log(Math.round(num3));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let num1 = 2.1;
+// let num2 = 2.2;
+
+// console.log(Math.ceil(num1));
+// console.log(Math.ceil(num2));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let num1 = 2.99;
+// let num2 = 2.66;
+
+// console.log(Math.floor(num1));
+// console.log(Math.floor(num2));
