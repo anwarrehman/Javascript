255be8334798bf47b10b928b29ec42cce62c5523
anwarrehman <ar1498137@gmail.com>
2024-06-25 04:14:17 -0700

    date & time methods

---
+++ chapter-32/app.js
@@ -0,0 +1,133 @@
+// let a = new Date();
+// let b = a.getDay();
+// console.log(b);
+
+
+
+
+
+
+
+
+// let a = new Date();
+// let b = a.getMonth();
+// console.log(b);
+
+
+
+
+
+
+
+
+
+// let a = new Date();
+// let b = a.getDate();
+// console.log(b);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let a = new Date();
+// let b = a.getFullYear();
+// console.log(b);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let a = new Date();
+// let b = a.getHours();
+// console.log(b);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let a = new Date();
+// let b = a.getMinutes();
+// console.log(b);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let a = new Date();
+// let b = a.getSeconds();
+// console.log(b);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let a = new Date();
+// let b = a.getMilliseconds();
+// console.log(b);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let a = new Date();
+// let b = a.getTime();
+// console.log(b);
