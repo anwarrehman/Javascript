1d17e18baac74cbcf8133b31d11520fb3e5b01c5
anwarrehman <ar1498137@gmail.com>
2024-06-25 04:05:50 -0700

    current date & time

---
+++ chapter-31/app.js
@@ -0,0 +1,22 @@
+// let now = new Date();
+// console.log(now);
+
+
+
+
+
+
+// let rightNow = new Date();
+// console.log(rightNow);
+
+
+
+
+
+
+
+
+
+
+// let now = new Date().toDateString().toUpperCase();
+// console.log(now);
