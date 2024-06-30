e9ef9a242c00ed9ef3d27d90f16c76920a056f22
anwarrehman <ar1498137@gmail.com>
2024-06-30 03:33:34 -0700

    specifying date & time

---
+++ chapter-33/app.js
@@ -0,0 +1,78 @@
+// let now = new Date("June 30, 2050");
+// console.log(now);
+
+
+
+
+
+// let now = new Date("October 15");
+// let time = now.getFullYear();
+// console.log(time);
+
+
+
+
+
+
+
+// let dob = new Date("October 26, 2005");
+// let now = new Date();
+
+// let age = now.getTime() - dob.getTime()
+
+// let yourAge = Math.floor(age / 1000 / 60 / 60 / 24 / 365.25);
+
+// console.log(yourAge);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let now = new Date();
+
+// let daysName = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+
+// let day = now.getDay();
+
+// console.log(day);
+
+// console.log(daysName[day]);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let now = new Date ();
+
+// let monthsName = ["Jan", "Feb", "Mar", "Apr", "May", "June", "July"];
+
+// let month = now.getMonth();
+
+// console.log(month);
+
+// console.log(monthsName[month]);
