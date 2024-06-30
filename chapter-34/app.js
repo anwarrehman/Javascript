a80a524e9b93c086df97b0eeef8cd182a8ea1c52
anwarrehman <ar1498137@gmail.com>
2024-06-30 03:43:11 -0700

    change elements of date & time

---
+++ chapter-34/app.js
@@ -0,0 +1,29 @@
+// let now = new Date();
+
+// now.setDate(15);
+
+// console.log(now);
+
+
+
+
+
+
+
+
+
+
+
+// let now = new Date();
+
+// now.setFullYear(2030);
+
+// now.setHours(13);
+
+// now.setMonth(2);
+
+// now.setSeconds(20);
+
+// now.setMilliseconds(1050);
+
+// console.log(now);
